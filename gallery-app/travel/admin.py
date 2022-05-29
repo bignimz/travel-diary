2d91f7fcda43629de2b5729c2a8eb3d7de227757
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-05-29 22:21:05 +0300

    Registered models in the admin

--- gallery-app/travel/admin.py
+++ gallery-app/travel/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Image, Category, Location, Tag
+
+admin.site.register(Image)
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(Tag)
+
