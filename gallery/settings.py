e187161db0fb8ec1415ffc71699d306f18d37d9e
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-05-30 13:58:29 +0300

    removed forward slash on images url path

--- gallery/settings.py
+++ gallery/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_FILES_DIRS = [os.path.join(BASE_DIR, 'static')],
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
